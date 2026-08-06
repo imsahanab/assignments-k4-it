3da56d354cf5e26d42e020c7f4dc78fc7a355a7a
sahana B <153760124+imsahanab@users.noreply.github.com>
2026-08-06 18:12:37 +0530

    Delete chat.module.ts

--- chat.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatService } from './chat.service';
-import { ChatGateway } from './chat.gateway';
-import { ChatRepository } from './repository/chat.repository';
-
-@Module({
-  providers: [ChatService, ChatGateway, ChatRepository],
-})
-export class ChatModule {}
